0b6a194646b42732709938f735f1b0aa3c91f505
danielPulido10 <33425807+danielPulido10@users.noreply.github.com>
2018-08-28 00:28:22 -0500

    Daniel

---
+++ Demanda_MRP1/src/Interfaces/MateriaPrima.java
@@ -0,0 +1,198 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+
+import java.awt.Color;
+import java.awt.Window;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author Danielpulido10
+ */
+public class MateriaPrima extends javax.swing.JPanel {
+
+    /**
+     * Creates new form MateriaPrima
+     */
+    public MateriaPrima() {
+        initComponents();
+        
+      MostrarMP.setBackground(new java.awt.Color(240,0,0));
+      MostrarMP.setForeground(Color.black);
+      SalirMP.setBackground(new java.awt.Color(0,0,0));
+      SalirMP.setForeground(Color.white);
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable3 = new javax.swing.JTable();
+        jLabel4 = new javax.swing.JLabel();
+        SalirMP = new javax.swing.JButton();
+        MostrarMP = new javax.swing.JButton();
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/coca-cola-41.gif"))); // NOI18N
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable2);
+
+        jTable3.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null},
+                {null},
+                {null},
+                {null}
+            },
+            new String [] {
+                "Proveedor"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane3.setViewportView(jTable3);
+
+        jLabel4.setText("Inventario Materia Prima");
+
+        SalirMP.setText("Salir");
+        SalirMP.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SalirMPActionPerformed(evt);
+            }
+        });
+
+        MostrarMP.setText("Mostrar Materia Prima");
+        MostrarMP.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MostrarMPActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(325, 325, 325)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(114, 114, 114)
+                        .addComponent(SalirMP))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(20, 20, 20)
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 392, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(112, 112, 112)
+                                .addComponent(jLabel4)))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(MostrarMP)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(15, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel4)
+                    .addComponent(MostrarMP))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 268, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(SalirMP)
+                .addContainerGap(14, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void SalirMPActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SalirMPActionPerformed
+        Window w = SwingUtilities.getWindowAncestor(MateriaPrima.this);
+        w.setVisible(false);
+    }//GEN-LAST:event_SalirMPActionPerformed
+
+    private void MostrarMPActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MostrarMPActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_MostrarMPActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton MostrarMP;
+    private javax.swing.JButton SalirMP;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable2;
+    private javax.swing.JTable jTable3;
+    // End of variables declaration//GEN-END:variables
+}
--- Demanda_MRP1/src/Interfaces/Principal.java
+++ Demanda_MRP1/src/Interfaces/Principal.java
@@ -1,11 +1,31 @@
 
 package Interfaces;
 
+import java.awt.Color;
+import javax.swing.JDialog;
+
 public class Principal extends javax.swing.JFrame {
 
    
     public Principal() {
         initComponents();
+        
+      this.getContentPane().setBackground(Color.white);
+     
+      DemandaBoton.setBackground(new java.awt.Color(0,0,0));
+      DemandaBoton.setForeground(Color.white);
+      ExplosionBoton.setBackground(new java.awt.Color(0,0,0));
+      ExplosionBoton.setForeground(Color.white);
+      HistorialBoton.setBackground(new java.awt.Color(0,0,0));
+      HistorialBoton.setForeground(Color.white);
+      PedidosBoton.setBackground(new java.awt.Color(0,0,0));
+      PedidosBoton.setForeground(Color.white);
+      MPBoton.setBackground(new java.awt.Color(0,0,0));
+      MPBoton.setForeground(Color.white);
+      InventarioBoton.setBackground(new java.awt.Color(0,0,0));
+      InventarioBoton.setForeground(Color.white);
+      
+     
     }
 
    
@@ -18,18 +38,29 @@ public class Principal extends javax.swing.JFrame {
         DemandaBoton = new javax.swing.JButton();
         PedidosBoton = new javax.swing.JButton();
         ExplosionBoton = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        InventarioBoton = new javax.swing.JButton();
+        MPBoton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBackground(new java.awt.Color(254, 0, 26));
+        setAutoRequestFocus(false);
+        setBackground(new java.awt.Color(255, 0, 51));
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         setForeground(java.awt.Color.gray);
 
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/coca-cola-41.gif"))); // NOI18N
-        jButton1.setEnabled(false);
 
         HistorialBoton.setText("Historial de Ventas");
+        HistorialBoton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                HistorialBotonMouseClicked(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                HistorialBotonMouseExited(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                HistorialBotonMouseEntered(evt);
+            }
+        });
         HistorialBoton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 HistorialBotonActionPerformed(evt);
@@ -37,6 +68,17 @@ public class Principal extends javax.swing.JFrame {
         });
 
         DemandaBoton.setText("Demanda ");
+        DemandaBoton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                DemandaBotonMouseClicked(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                DemandaBotonMouseExited(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                DemandaBotonMouseEntered(evt);
+            }
+        });
         DemandaBoton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 DemandaBotonActionPerformed(evt);
@@ -44,6 +86,14 @@ public class Principal extends javax.swing.JFrame {
         });
 
         PedidosBoton.setText("Pedidos materia prima");
+        PedidosBoton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                PedidosBotonMouseExited(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                PedidosBotonMouseEntered(evt);
+            }
+        });
         PedidosBoton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 PedidosBotonActionPerformed(evt);
@@ -51,18 +101,47 @@ public class Principal extends javax.swing.JFrame {
         });
 
         ExplosionBoton.setText("Explosión de Materiales");
+        ExplosionBoton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                ExplosionBotonMouseExited(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                ExplosionBotonMouseEntered(evt);
+            }
+        });
         ExplosionBoton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 ExplosionBotonActionPerformed(evt);
             }
         });
 
-        jButton2.setText("jButton2");
-
-        jButton3.setText("jButton3");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        InventarioBoton.setText("Inventario Producto");
+        InventarioBoton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                InventarioBotonMouseExited(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                InventarioBotonMouseEntered(evt);
+            }
+        });
+        InventarioBoton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                InventarioBotonActionPerformed(evt);
+            }
+        });
+
+        MPBoton.setText("Inventario Materia Prima");
+        MPBoton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                MPBotonMouseExited(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                MPBotonMouseEntered(evt);
+            }
+        });
+        MPBoton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MPBotonActionPerformed(evt);
             }
         });
 
@@ -72,7 +151,7 @@ public class Principal extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(23, 23, 23)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(ExplosionBoton, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(47, 47, 47)
@@ -81,11 +160,11 @@ public class Principal extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(DemandaBoton, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(HistorialBoton, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(92, 92, 92)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton3)
-                            .addComponent(jButton2))))
-                .addContainerGap(41, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(InventarioBoton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(MPBoton, javax.swing.GroupLayout.DEFAULT_SIZE, 204, Short.MAX_VALUE))))
+                .addContainerGap(43, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -97,50 +176,139 @@ public class Principal extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(36, 36, 36)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(HistorialBoton, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton3))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(HistorialBoton, javax.swing.GroupLayout.DEFAULT_SIZE, 56, Short.MAX_VALUE)
+                    .addComponent(MPBoton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(DemandaBoton, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(DemandaBoton, javax.swing.GroupLayout.DEFAULT_SIZE, 56, Short.MAX_VALUE)
+                    .addComponent(InventarioBoton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(26, 26, 26)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(ExplosionBoton, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(PedidosBoton, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(109, Short.MAX_VALUE))
+                .addContainerGap(36, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void HistorialBotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_HistorialBotonActionPerformed
-    Historial p = new Historial();
-    Principal f = new Principal();
-    f.getContentPane().add(p);
+  JDialog dialogo = new JDialog(this,"Historial");
+    Historial panel = new Historial();
+    dialogo.add(panel);
+    dialogo.setVisible(true);
+    dialogo.setSize(780, 500);
     }//GEN-LAST:event_HistorialBotonActionPerformed
 
     private void DemandaBotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DemandaBotonActionPerformed
-    Demanda p = new Demanda();
-    Principal f = new Principal();
-    f.getContentPane().add(p);
+   JDialog dialogo = new JDialog(this,"Demanda");
+    Demanda panel = new Demanda();
+    dialogo.add(panel);
+    dialogo.setVisible(true);
+    dialogo.setSize(780, 500);
+
     }//GEN-LAST:event_DemandaBotonActionPerformed
 
     private void PedidosBotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PedidosBotonActionPerformed
-    Pedido p = new Pedido();
-    Principal f = new Principal();
-    f.getContentPane().add(p);
+    JDialog dialogo = new JDialog(this,"Pedidos");
+    Pedido panel = new Pedido();
+    dialogo.add(panel);
+    dialogo.setVisible(true);
+    dialogo.setSize(580, 430);
     }//GEN-LAST:event_PedidosBotonActionPerformed
 
     private void ExplosionBotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ExplosionBotonActionPerformed
-    Explosion p = new Explosion();
-    Principal f = new Principal();
-    f.getContentPane().add(p);
+    JDialog dialogo = new JDialog(this,"Explosion de Materiales");
+    Explosion panel = new Explosion();
+    dialogo.add(panel);
+    dialogo.setVisible(true);
+    dialogo.setSize(780, 500);
     }//GEN-LAST:event_ExplosionBotonActionPerformed
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+    private void MPBotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MPBotonActionPerformed
+    JDialog dialogo = new JDialog(this,"Inventario Materia Prima");
+    MateriaPrima panel = new MateriaPrima();
+    dialogo.add(panel);
+    dialogo.setVisible(true);
+    dialogo.setSize(840, 500);   
+    }//GEN-LAST:event_MPBotonActionPerformed
+
+    private void HistorialBotonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_HistorialBotonMouseEntered
+      HistorialBoton.setBackground(new java.awt.Color(244,0,0));
+      HistorialBoton.setForeground(Color.black);
+    }//GEN-LAST:event_HistorialBotonMouseEntered
+
+    private void HistorialBotonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_HistorialBotonMouseExited
+      HistorialBoton.setBackground(new java.awt.Color(0,0,0));
+      HistorialBoton.setForeground(Color.white);
+    }//GEN-LAST:event_HistorialBotonMouseExited
+
+    private void DemandaBotonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_DemandaBotonMouseEntered
+       DemandaBoton.setBackground(new java.awt.Color(244,0,0));
+       DemandaBoton.setForeground(Color.black);
+    }//GEN-LAST:event_DemandaBotonMouseEntered
+
+    private void DemandaBotonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_DemandaBotonMouseExited
+       DemandaBoton.setBackground(new java.awt.Color(0,0,0));
+       DemandaBoton.setForeground(Color.white);
+    }//GEN-LAST:event_DemandaBotonMouseExited
+
+    private void ExplosionBotonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ExplosionBotonMouseEntered
+       ExplosionBoton.setBackground(new java.awt.Color(244,0,0));
+       ExplosionBoton.setForeground(Color.black);
+    }//GEN-LAST:event_ExplosionBotonMouseEntered
+
+    private void ExplosionBotonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ExplosionBotonMouseExited
+       ExplosionBoton.setBackground(new java.awt.Color(0,0,0));
+       ExplosionBoton.setForeground(Color.white);
+    }//GEN-LAST:event_ExplosionBotonMouseExited
+
+    private void MPBotonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_MPBotonMouseEntered
+       MPBoton.setBackground(new java.awt.Color(244,0,0));
+       MPBoton.setForeground(Color.black);
+    }//GEN-LAST:event_MPBotonMouseEntered
+
+    private void MPBotonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_MPBotonMouseExited
+       MPBoton.setBackground(new java.awt.Color(0,0,0));
+       MPBoton.setForeground(Color.white);
+    }//GEN-LAST:event_MPBotonMouseExited
+
+    private void InventarioBotonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_InventarioBotonMouseEntered
+       InventarioBoton.setBackground(new java.awt.Color(244,0,0));
+       InventarioBoton.setForeground(Color.black);
+    }//GEN-LAST:event_InventarioBotonMouseEntered
+
+    private void InventarioBotonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_InventarioBotonMouseExited
+       InventarioBoton.setBackground(new java.awt.Color(0,0,0));
+       InventarioBoton.setForeground(Color.white);
+    }//GEN-LAST:event_InventarioBotonMouseExited
+
+    private void PedidosBotonMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_PedidosBotonMouseEntered
+       PedidosBoton.setBackground(new java.awt.Color(244,0,0));
+       PedidosBoton.setForeground(Color.black);
+    }//GEN-LAST:event_PedidosBotonMouseEntered
+
+    private void PedidosBotonMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_PedidosBotonMouseExited
+       PedidosBoton.setBackground(new java.awt.Color(0,0,0));
+       PedidosBoton.setForeground(Color.white);
+    }//GEN-LAST:event_PedidosBotonMouseExited
+
+    private void HistorialBotonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_HistorialBotonMouseClicked
+       
+    }//GEN-LAST:event_HistorialBotonMouseClicked
+
+    private void DemandaBotonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_DemandaBotonMouseClicked
+      
+    }//GEN-LAST:event_DemandaBotonMouseClicked
+
+    private void InventarioBotonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InventarioBotonActionPerformed
+    JDialog dialogo = new JDialog(this,"Inventario Producto");
+    Producto panel = new Producto();
+    dialogo.add(panel);
+    dialogo.setVisible(true);
+    dialogo.setSize(800, 500);   
+    }//GEN-LAST:event_InventarioBotonActionPerformed
 
   
     public static void main(String args[]) {
@@ -179,9 +347,9 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JButton DemandaBoton;
     private javax.swing.JButton ExplosionBoton;
     private javax.swing.JButton HistorialBoton;
+    private javax.swing.JButton InventarioBoton;
+    private javax.swing.JButton MPBoton;
     private javax.swing.JButton PedidosBoton;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
     // End of variables declaration//GEN-END:variables
 }
